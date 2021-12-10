f2ebdfbed2cf8b3fc4f0eaa964bb54949e26871d
Puscasu Eduard-Vasilica <eduard-vasilica.puscasu@student.tuiasi.ro>
2021-12-09 20:10:53 -0800

    scheletul functiei CONNECT

--- main.py
+++ main.py
@@ -38,11 +38,135 @@ def decodeSecondByte(byte):
         return i + 1, val
 
 
-
+def decode_binary_flag(flag):
+    flags = str(format(flag, 'b'))
+    ret = []
+    if len(flags) != 8:
+        for i in range(8 - len(flags)):
+            ret.append(0)
+    for i in range(len(flags)):
+        ret.append(int(flags[i]))
+    return ret
 
 
 def CONNECT(client, data):
-    print(data);
+    #acum luam fiecare byte si descifram mesajul
+    #https://docs.python.org/3/library/struct.html pentru struct unpack
+    lenght_protocol_name =(struct.unpack('>H',data[0:2])[0])
+    print(lenght_protocol_name)
+    if lenght_protocol_name!= 4:
+        print("Lungimea numelui gresita!!")
+        return 0
+    protocol_name = data[2:2+lenght_protocol_name]
+    data = data[2+lenght_protocol_name:]
+    print(protocol_name)
+    protocol_name = protocol_name.decode("utf-8")
+
+
+    #print(prtocol_name =struct.unpack( '!6s', data[2:lenght_protocol_name]).decode("utf-8"))
+    #verificam numele protocolului
+    if protocol_name != 'mqtt' and protocol_name != 'MQTT':
+      print("Nume protocol gresit")
+      return 0
+    protocol_lvl =struct.unpack('>b',data[0:1])[0]
+    print(protocol_lvl)
+
+    #verificam protocol lvl
+
+    if(protocol_lvl!=4):
+        print("Protocol lvl not 4")
+        return 0
+    data = data[1:]         #update data
+    flag = struct.unpack('>B', data[0:1])[0]
+    data =data[1:]          #update data
+    flags = decode_binary_flag(flag)
+
+    #verificare bit reserved
+    if( flags[7]!=0):
+        print("Bit rezerved pe 1 ")
+        return 0
+
+    if(flag[6]== 0 ):
+        #presupunem ca este 1 ca sa nu ne complicam momentan
+        print("face ceva")
+
+
+
+
+        # verificcam ce fel de qos avem
+    if(flag[4]==flag[3]==0):
+        print("Folosim qos 0")
+    elif(flag[4]== 1 and flag[3] ==0):
+        print("Folosim qos 1")
+    elif(flag[4]==0 and flag[3] == 1):
+        print("Folosim qos 2")
+
+
+    if(flag[2]!=0):
+        print("Fara will-retain")
+
+    keep_alive = (struct.unpack('>H', data[0:2])[0])
+
+    data = data[2:]         #update data
+    if not(keep_alive):
+        print("nu are keep_alive")
+    ####
+    ###  incepe payloadul de aici
+    ###
+
+
+    data = data[2:]         #update data
+    if not(keep_alive):
+        print("nu are keep_alive")
+
+
+    #luam id ul clientului
+    id_length = (struct.unpack('>H', data[0:2])[0])
+    id_name = data[2:2 +id_length]
+    data =data[2+id_length :]
+    id_name = id_name.decode("utf-8")
+
+
+    #verificam daca are topic
+    if(flag[5]):
+        will_lenght = (struct.unpack('>H', data[0:2])[0])
+        will_topic = data[2:2+will_lenght]
+        data = data[2+will_lenght]
+        will_topic = will_topic.decode("utf-8")
+        print("are si topic")
+
+
+    if  (flag[5]):
+        #implementam cum este precizat in documentatie"
+        will_lenght = (struct.unpack('>H', data[0:2])[0])
+        will_message= data[2:2 + will_lenght]
+        data = data[2 + will_lenght]
+        will_message = will_message.decode("utf-8")
+        print("are si will_message")
+
+    if(flag[0]):
+        name_length = (struct.unpack('>H', data[0:2])[0])
+        name = data[2:2 + name_length]
+        data = data[2 + will_lenght]
+        name = name.decode("utf-8")
+        if(name != 'nume'):
+            print("Username gresit")
+            return 0
+        print("are si nume")
+
+    if(flag[1]):
+        password_length = (struct.unpack('>H', data[0:2])[0])
+        password = data[2:2 + name_length]
+        data = data[2 + will_lenght]
+        password = password.decode("utf-8")
+        if (password != 'parola'):
+            print("Parola gresita")
+            return 0
+        print("are si parola")
+
+    return 1
+
+
 
 
 
